8af4a9f8c76036017e64a86cb3c7aae49956bce5
FrancescoBonandin <bonandinfrancesco@gmail.com>
2024-01-09 18:21:52 +0100

    fixed data recovery for related data

--- app/Http/Controllers/api/FlightController.php
+++ app/Http/Controllers/api/FlightController.php
@@ -13,14 +13,23 @@ class FlightController extends Controller
 
         $departureAirport= Airport::where('name', $request->input('departure-airport'))->firstOrFail();
         $arrivalAirport=Airport::where('name', $request->input('arrival-airport'))->firstOrFail();
-        $departure_date=date($request->input('departure-date', now()));
+        $departureDate=date($request->input('departure-date', now()));
 
+        if($departureAirport!=$arrivalAirport){
 
-        $flights=Flight::where('departure_airport_id', $departureAirport->id)
-                         ->where('arrival_airport_id', $arrivalAirport->id)
-                         ->where('departure_datetime', '>', $departure_date)
-                         ->orderBy('departure_datetime', 'ASC')
-                         ->get();
-        return response()->json(['flights'=>$flights], 200);
+            $flights=Flight::with(['departureAirport', 'arrivalAirport'])
+                             ->where('departure_airport_id', $departureAirport->id)
+                             ->where('arrival_airport_id', $arrivalAirport->id)
+                             ->where('departure_datetime', '<', $departureDate)
+                             ->orderBy('departure_datetime', 'ASC')
+                             ->get();
+            return response()->json(['flights'=>$flights], 200);
+
+        }
+
+        else{
+            // MAKE A CHOICE ON WHAT TO RESPOND IN CASE OF ERROR
+        }
+
     }
 }
--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Flight extends Model
 {
     use HasFactory;
+
+  public function departureAirport(){
+
+    return $this->belongsTo(Airport::class, 'departure_airport_id', 'id');
+    
+  }
+
+  public function arrivalAirport(){
+
+    return $this->belongsTo(Airport::class, 'arrival_airport_id', 'id');
+    
+  }
+
 }
